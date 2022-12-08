c11eb8cb79c027b8196117c1edf8bebc17302dcf
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2022-12-08 07:48:11 -0900

    Change timestamps snake_case to camelCase
    - Change underscored timestamps to camelCase in indexConversation
    - Change underscored timestamps to camelCase in user, friendship, and conversation migration files

--- src/migrations/20221129180710-create-participant.js
+++ src/migrations/20221129180710-create-participant.js
@@ -32,11 +32,11 @@ export const up = async (queryInterface, Sequelize) => {
       allowNull: false,
       type: Sequelize.STRING,
     },
-    created_at: {
+    createdAt: {
       allowNull: false,
       type: Sequelize.DATE,
     },
-    updated_at: {
+    updatedAt: {
       allowNull: false,
       type: Sequelize.DATE,
     },
