a60d30c6b9aaf8c8b7494a8457a01e36af215a16
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-01-11 10:03:59 +0300

    Fix extra properties in userResource
    - Remove email and updatedAt in userResource
    - Add excplicitly email and updatedAt in whoami response

--- src/resources/userResource.js
+++ src/resources/userResource.js
@@ -6,11 +6,9 @@ const userResource = (resource) => {
       first: resource.firstName,
       last: resource.lastName,
     },
-    email: resource.email,
     imageUrl: resource.imageUrl,
     description: resource.description,
     createdAt: resource.createdAt,
-    updatedAt: resource.updatedAt,
   };
 };
 
