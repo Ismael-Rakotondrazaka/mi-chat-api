5a8111488f3bb58e08ea979028a29653ba2366ba
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-01-11 09:31:54 +0300

    Fix wrong friendship in indexUserFriend
    - Create two different functions to handle auth and unauth request
    
    TODO:
    - Create more query prameters features

--- src/controllers/users/indexUserFriend.js
+++ src/controllers/users/indexUserFriend.js
@@ -1,4 +1,4 @@
-import { userResource, friendResource } from "#resources/index.js";
+import { userResource, friendResource, userCollection } from "#resources/index.js";
 import { User } from "#models/index.js";
 import { createDataResponse } from "#utils/responses/index.js";
 
@@ -10,102 +10,80 @@ const indexUserFriend = async (req, res, next) => {
      the results depends on the auth state, so we create functions to handle it for both case
     */
 
-  let { limit, order, like, count, mutual } = req.query;
+     // TODO add more query features
+  let { /* limit, order, like, count, */ mutual } = req.query;
   const targetUserId = +req.params.userId;
 
-  let userParams = {
-    include: [
-      {
+  const processWithAuth = async (targetUserId = 0, authUserId = 0) => {
+    /*
+        we get with the targetUser with:
+        - his friends,
+        - the friendship between his friends and the auth user
+      */
+    const targetUser = await User.findByPk(targetUserId, {
+      include: {
         association: "Friends",
+        include: {
+          association: "Friends",
+          where: {
+            "$Friends.Friends.id$": {
+              [Op.eq]: authUserId,
+            },
+          },
+          required: !!mutual,
+        },
       },
-    ],
-    where: {},
-  };
-
-  const processData = async () => {
-    const targetUser = await User.findByPk(targetUserId);
-
-    const friends = await targetUser.getFriends(userParams);
-
-    const result = friends.map((friend) => {
-      let friendObject;
-
-      if (friend?.Friends?.length === 1) {
-        // ! we explicitly add a Friendship property, so DON'T save it
-        friend.Friendship = friend.Friends.at(0).Friendship;
-
-        friendObject = friendResource(friend);
-      } else {
-        friendObject = userResource(friend);
-
-        friendObject.friendship = {
-          isFriend: false,
-        };
-      }
-
-      return friendObject;
     });
 
-    const response = {
-      users: result,
-    };
+    let result = {
+      users: targetUser.Friends.map((friend) => {
+        let friendObject = {};
 
-    if (count === true || count === "true") {
-      // we delete the limit because count is independent of limit
-      if (Object.hasOwnProperty.call(userParams, "limit")) {
-        delete userParams.limit;
-      }
+        if (friend.Friends.length === 1) {
+          // ! we add explicitly a new property, so DON'T save it
+          friend.Friendship = friend.Friends.at(0).Friendship;
+          friendObject = friendResource(friend);
+        } else {
+          friendObject = userResource(friend);
 
-      // we only need the length, so we get the minimum informations
-      userParams.attributes = ["id"];
-
-      const friends = await targetUser.getFriends(userParams);
-
-      response.count = friends.length;
-    }
-
-    return response;
-  };
-
-  try {
-    const maxLimit = 200;
-
-    if (/^\d+$/.test(limit)) {
-      limit = +limit;
-      limit = limit < maxLimit ? limit : maxLimit;
-      userParams.limit = limit;
-    }
-
-    userParams.order =
-      order === "DESC" ? [["firstName", "DESC"]] : [["firstName", "ASC"]];
-
-    if (like && (like + "").trim()) {
-      const likeTrimmed = (like + "").trim();
-      if (/^[\p{L}\p{M} ]+$/u.test(likeTrimmed)) {
-        userParams.where[Op.or] = [
-          {
-            "$User.first_name$": {
-              [Op.substring]: likeTrimmed,
-            },
-          },
-          {
-            "$User.last_name$": {
-              [Op.substring]: likeTrimmed,
-            },
-          },
-        ];
-      } else {
-        const result = {
-          users: [],
-        };
-
-        if (count === true || count === "true") {
-          result.count = 0;
+          friendObject.friendship = {
+            isFriend: false,
+          };
         }
 
-        return res.json(createDataResponse(result));
-      }
-    }
+        return friendObject;
+      }),
+    };
+
+    return result;
+  };
+
+  const processWithoutAuth = async (targetUserId = 0) => {
+    const targetUser = await User.findByPk(targetUserId, {
+      include: {
+        association: "Friends",
+      },
+    });
+
+    // explicitly add a falsy friendship because the request is not authenticated
+    let result = {
+      users: userCollection(targetUser.Friends).map((user) => {
+        user.friendship = {
+          isFriend: false,
+        };
+
+        return user;
+      }),
+    };
+
+    return result;
+  };
+
+  let isAuthenticated = false;
+  // const targetUserId = +req.params.userId;
+
+  try {
+    let authUserId;
 
     // we check if the user is authenticated
     const authHeader = req.headers.authorization || req.headers.Authorization;
@@ -134,24 +112,23 @@ const indexUserFriend = async (req, res, next) => {
           });
 
           if (authUser) {
-            userParams.include = [
-              {
-                association: "Friends",
-                attributes: ["id"],
-                where: {
-                  "$Friends.id$": authUser.id,
-                },
-                required: !!mutual,
-              },
-            ];
+            isAuthenticated = true;
+            authUserId = authUser.id;
           }
         }
       }
     }
 
-    let response = await processData();
+    let result;
 
-    return res.json(createDataResponse(response));
+    // authenticated
+    if (isAuthenticated) {
+      result = await processWithAuth(targetUserId, authUserId);
+    } else {
+      result = await processWithoutAuth(targetUserId);
+    }
+
+    return res.json(createDataResponse(result));
   } catch (error) {
     /*
         we can have a jwt Error here, that means the auth is failed,
@@ -161,9 +138,9 @@ const indexUserFriend = async (req, res, next) => {
     if (error instanceof jwt.JsonWebTokenError) {
       req.payload = null;
 
-      return await processData()
-        .then((response) => {
-          return res.json(createDataResponse(response));
+      return await processWithoutAuth(targetUserId)
+        .then((result) => {
+          return res.json(createDataResponse(result));
         })
         .catch((error) => {
           next(error);
